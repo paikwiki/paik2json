a127e8baa5e62bd55ba337b8671ad6b86eceeb9a
paikwiki <paikwiki@gmail.com>
2023-12-25 02:27:54 +0900

    :pencil2: Raw_data에 strip() 미적용

--- paik2json/line/line_manager.py
+++ paik2json/line/line_manager.py
@@ -3,5 +3,5 @@ from .line import Line
 
 class LineManager:
     def __init__(self, raw_data: str):
-        self.lines = [Line(line) for line in raw_data.strip().splitlines()]
+        self.lines = [Line(line) for line in raw_data.splitlines()]
         self.deepest_depth = max([line.depth for line in self.lines])
