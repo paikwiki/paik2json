4e4410f93bfb9f3683dd289923fbc2ebecfec527
paikwiki <paikwiki@gmail.com>
2024-01-05 23:23:08 +0900

    :bookmark: Ver.0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="paik2json",
-    version="0.0.4",
+    version="0.1.0",
     description="convert paik to json",
     author="paikwiki",
     author_email="paikwiki@gmail.com",
